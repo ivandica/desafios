9b70134228e15e3e47a55f1f0fb07451a31f58e9
ivandica <ivandica@gmail.com>
2017-09-20 00:45:32 -0300

    Add files via upload

--- crawlers/Crawler.java
+++ crawlers/Crawler.java
@@ -25,6 +25,12 @@ public class Crawler {
 	public static void main(String[] args) 
 	{
 		String line = "askreddit;worldnews;cats";
+		boolean isRobo = false;
+		if (line.substring(0, 13).equals("/NadaPraFazer")) {
+			isRobo = true;
+			line = line.substring(14);
+		} 
+		
 		List<CrawlerDados> dados = new ArrayList<CrawlerDados>();
 		
 		List<String> threads = new ArrayList<String>();
@@ -65,31 +71,33 @@ public class Crawler {
 		
     	driver.quit();
     	
-    	TelegramBot bot = TelegramBotAdapter.build("444095557:AAEAqVcL9KnGCLP-Jf--QxEvwZ3XvXmS9Ps");
-
-    	GetUpdatesResponse updatesResponse;
-    	SendResponse sendResponse;
-    	BaseResponse baseResponse;
-    			
-    	int m = 0;
-    			
-		updatesResponse =  bot.execute(new GetUpdates().limit(100).offset(m));
-		
-		List<Update> updates = updatesResponse.updates();
-
-		for (Update update : updates) {
+    	if (isRobo) {
+	    	TelegramBot bot = TelegramBotAdapter.build("444095557:AAEAqVcL9KnGCLP-Jf--QxEvwZ3XvXmS9Ps");
+	
+	    	GetUpdatesResponse updatesResponse;
+	    	SendResponse sendResponse;
+	    	BaseResponse baseResponse;
+	    			
+	    	int m = 0;
+	    			
+			updatesResponse =  bot.execute(new GetUpdates().limit(100).offset(m));
 			
-			m = update.updateId() + 1;
-			
-			System.out.println("Recebendo mensagem: " + update.message().text());
-			
-			baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
-			System.out.println("Resposta de Chat Action Enviada? " + baseResponse.isOk());
-			
-			Gson gson = new Gson();
-	        String json = gson.toJson(dados);
-	        sendResponse = bot.execute(new SendMessage(update.message().chat().id(), json));
-			System.out.println("Mensagem Enviada? " + sendResponse.isOk());
-		}
+			List<Update> updates = updatesResponse.updates();
+	
+			for (Update update : updates) {
+				
+				m = update.updateId() + 1;
+				
+				System.out.println("Recebendo mensagem: " + update.message().text());
+				
+				baseResponse = bot.execute(new SendChatAction(update.message().chat().id(), ChatAction.typing.name()));
+				System.out.println("Resposta de Chat Action Enviada? " + baseResponse.isOk());
+				
+				Gson gson = new Gson();
+		        String json = gson.toJson(dados);
+		        sendResponse = bot.execute(new SendMessage(update.message().chat().id(), json));
+				System.out.println("Mensagem Enviada? " + sendResponse.isOk());
+			}
+    	}
 	}
 }
